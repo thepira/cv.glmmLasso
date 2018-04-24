39226691ef7b20d3af8f6b15434f72e3af989b7a
Pira <>
2018-04-24 19:13:44 -0400

    built glmmLasso_MultLambdas function as an abstraction of the cross validation function. cv.glmmLasso will later call this function to do the cross validation.

---
+++ R/glmmLasso_MultLambdas.r
@@ -0,0 +1,97 @@
+
+#' @title glmmLasso_MultLambdas
+#' @description Variable selection using glmmLasso for multiple lambdas values  
+#' @details Build multiple models given a sequence of lambda values
+#' @author Pira Jitngamplang, Jared Lander
+#' @export
+#' @param fix A two-sided linear formula object describing the fixed-effects part of the model, with the response on the left of a ~ operator and the terms, separated by + operators, on the right. For categorical covariables use as.factor(.) in the formula. Note, that the corresponding dummies are treated as a group and are updated blockwise
+#' @param rnd A two-sided linear formula object describing the random-effects part of the model, with the grouping factor on the left of a ~ operator and the random terms, separated by + operators, on the right; aternatively, the random effects design matrix can be given directly (with suitable column names). If set to NULL, no random effects are included.
+#' @param data The data frame containing the variables named in formula.
+#' @param family a GLM family, see glm and family. Also ordinal response models can be fitted: use family=acat() and family=cumulative() for the fitting of an adjacent category or cumulative model, respectively. If family is missing then a linear mixed model is fit; otherwise a generalized linear mixed model is fit.
+#' @param lambda The penalty parameter that controls the shrinkage of fixed terms and controls the variable selection. The optimal penalty parameter is a tuning parameter of the procedure that has to be determined, e.g. by use of information criteria or cross validation. Should inputted as a numeric vector from high to low. (See details for an example.)
+#' @return Returns a list glmmLasso models for each lambda value.  
+#' @examples
+#' 
+#' linear mixed model with slope on ave.attend
+#' 
+#' glmmLasso_MultLambdas(fix = points ~ transfer.spendings ball.possession + ave.attend, rnd = list(team =~ 1 + ave.attend), data = soccer, family = poisson(link = log), lambda = seq(from = 500, to = 1, by = -5))
+#' 
+#'  
+
+glmmLasso_MultLambdas <- function(fix, rnd, data, family, lambdas, ...)
+{
+
+    # instantiating list object to hold the model outputs 
+    modList <- vector(mode = 'list', length = length(lambdas))
+    
+    # fitting first model to generate initial inputs for control parameter
+    # here we use the first lambda (highest penalty) to start
+    # based glmmLasso's author, glmmLasso is faster when final coefficient
+    # estimates corresponding to a lambda is used as the starting value for
+    # the next smaller lambda  
+    
+    mod1 <- glmmLasso::glmmLasso(fix = fix,
+                                 rnd = rnd,
+                                 data = data,
+                                 family = family,
+                                 lambda = lambdas[1],
+                                 ...)
+    
+    # Delta is matrix containing the estimates of fixed and random effects 
+    # (columns) for each iteration (rows) of the main algorithm (i.e. before 
+    # the final re-estimation step is performed, see details).
+    # Passing the set of estimates from the last iteration as the 
+    # 'start' parameter of the controlList
+    Delta.start <- mod1$Deltamatrix[mod1$conv.step, ]
+    
+    # Q_long is a list containing the estimates of the random effects 
+    # variance-covariance parameters for each iteration of the main algorithm.
+    # Passing the variance-covaiance matrix as the q_start parameter of
+    # the controlList 
+    Q.start <- mod1$Q_long[[mod1$conv.step + 1]]
+    
+    # building the controlList as the first starting control parameter for loop
+    controlList <- list(start=Delta.start, q_start=Q.start)
+   
+    # calling glmmLasso for each lambda value and after each fit, controlList
+    # gets updated with the lastest model's coefficient to increase speed
+        for (l in seq_along(lambdas))
+    {
+        modList[[l]] <- glmmLasso::glmmLasso(fix = fix,
+                                           rnd = rnd,
+                                           data = data,
+                                           family = family,
+                                           lambda = lambdas[l],
+                                           control = controlList,
+                                           ...)
+        
+        Delta.start <- modList[[l]]$Deltamatrix[modList[[l]]$conv.step, ]
+        Q.start <- modList[[l]]$Q_long[[modList[[l]]$conv.step + 1]]
+        controlList <- list(start=Delta.start,q_start=Q.start)
+        
+    }
+    
+    # the function returns  a list of glmmLasso models 
+    return(modList)
+
+}
+
+library(glmmLasso)
+data("soccer")
+## generalized additive mixed model
+## grid for the smoothing parameter
+
+## center all metric variables so that also the starting values with glmmPQL are in the correct scaling
+
+soccer[,c(4,5,9:16)]<-scale(soccer[,c(4,5,9:16)],center=T,scale=T)
+soccer<-data.frame(soccer)
+
+
+glmmLasso_MultLambdas(fix = points ~ transfer.spendings + ave.unfair.score + ball.possession + tackles + ave.attend + sold.out, 
+                  rnd = list(team =~ 1 + ave.attend),
+                  data = soccer, 
+                  family = poisson(link = log), 
+                  lambda = seq(from = 500, to = 1, by = -5))
+    
+
+
