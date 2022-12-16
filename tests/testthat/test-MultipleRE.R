c2cba655f16b608b0d4833016201bf3e5a5712ab
mollie brooks <mollieebrooks@gmail.com>
2022-12-16 10:14:59 +0100

    tiny fix

--- tests/testthat/test-MultipleRE.R
+++ tests/testthat/test-MultipleRE.R
@@ -2,11 +2,11 @@ context("MultipleRandomEffects")
 data("soccer", package = "glmmLasso")
 soccer <- transform(soccer, teamf=factor(team), posf=factor(pos))
 
-lambdas=seq(50, 0, by=-5)
 
 mod2RE <- cv.glmmLasso(fix = points ~ transfer.spendings + ave.unfair.score + 
-                         ball.possession + tackles, rnd = list(teamf=~1, posf=~1), data = soccer, 
-                     family = gaussian(link = "identity"), kfold = 5, lambda.final = 'lambda.1se')
+                           ball.possession + tackles, rnd = list(teamf=~1, posf=~1), data = soccer, 
+                       family = gaussian(link = "identity"), kfold = 5, lambda.final = 'lambda.1se',
+                       lambdas=seq(50, 0, by=-5))
 
 test_that("Final model contains specified number of random effects", {
     expect_equal(length(mod2RE$glmmLasso.final$Q), 2)
